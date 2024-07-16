77cac8056d7dfa87ce1ee1ac4536cdc0c64e9a66
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-07-16 15:11:28 +0530

    feat: Add user signup functionality and page using prisma

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,19 +1,27 @@
 import { NextRequest } from "next/server"
+import { PrismaClient } from "@prisma/client";
 
-// export function GET(){
-//     //database logic 
+const client = new PrismaClient(); 
 
-//     return  Response.json({
-//         name:"John Doe",
-//         email:"John@gmail.com"
-//     })
-// }
+export function GET(){
+    //database logic 
+
+    return  Response.json({
+        name:"John Doe",
+        email:"John@gmail.com"
+    })
+}
 export async function POST(req:NextRequest){
     //database logic 
     //extract the body
     const body = await req.json();
+    await client.user.create({
+        data:{
+            username:body.username,
+            password:body.password
+        }
+    })
     //store the body in  the db
-    console.log(body);
 
     return  Response.json({
         message:'user logged in'
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import axios from "axios";
 
 async function getUserData(){
-  await new Promise((r)=>setTimeout(r,5000)) 
+  await new Promise((r)=>setTimeout(r,2000)) 
   const response = await axios.get("http://localhost:3000/api/user")
   return response.data;
 }

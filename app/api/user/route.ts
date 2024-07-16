a4745ecccb44ebafe3c719f51c33db2636efa393
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-07-16 14:20:45 +0530

    chore: Add user signup functionality and page

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,16 +1,21 @@
-export function GET(){
+import { NextRequest } from "next/server"
+
+// export function GET(){
+//     //database logic 
+
+//     return  Response.json({
+//         name:"John Doe",
+//         email:"John@gmail.com"
+//     })
+// }
+export async function POST(req:NextRequest){
     //database logic 
+    //extract the body
+    const body = await req.json();
+    //store the body in  the db
+    console.log(body);
 
     return  Response.json({
-        name:"John Doe",
-        email:"John@gmail.com"
-    })
-}
-export function POST(){
-    //database logic 
-
-    return  Response.json({
-        name:"John Doe",
-        email:"John"
+        message:'user logged in'
     })
 }

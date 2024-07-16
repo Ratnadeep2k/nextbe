7067bf29292fb0620e7e1d0267c96e8641cbdfd2
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-07-16 14:03:14 +0530

    first commit

---
+++ app/api/user/route.ts
@@ -0,0 +1,16 @@
+export function GET(){
+    //database logic 
+
+    return  Response.json({
+        name:"John Doe",
+        email:"John@gmail.com"
+    })
+}
+export function POST(){
+    //database logic 
+
+    return  Response.json({
+        name:"John Doe",
+        email:"John"
+    })
+}
